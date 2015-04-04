1b874ccf2bbc48bcbd49f879440db6e6aad5d73f
Kathy Saad <kathysaad@gmail.com>
2015-04-04 04:16:11 +0000

    Wrote a bash script to save me time when I start up VMs using Vagrant

---
+++ vagrant-machine-setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Vagrant machine setup script
+# I wrote this so that every time my data gets wiped off the machine,
+# I can easily run this script and be ready to do things that matter
+# and save a lot of time.
+
+sudo apt-get install git
+sudo apt-get update
+# The above line is necessary for the following line to work
+sudo apt-get install vim
